cb96b27c89f19ff44386a8567266292b7ee1e969
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-26 03:22:40 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -5,3 +5,11 @@ document.getElementById('welcomeText').addEventListener('click', function() {
     }, 500); // Adjust timing as needed
 });
 
+// Reset the opacity when the page is shown, e.g., navigating back
+window.onpageshow = function(event) {
+    // Check if the page was accessed via the back button
+    if (event.persisted || (window.performance && window.performance.navigation.type == 2)) {
+        // Reset the container's opacity
+        document.querySelector('.container').style.opacity = '1';
+    }
+};
