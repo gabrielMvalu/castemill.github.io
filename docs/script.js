4f911e8a824cf90a7ef7189d5924a71dce1c4355
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-26 03:00:30 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,14 @@
 document.getElementById('welcomeText').addEventListener('click', function() {
-    document.getElementById('mainContent').classList.remove('hidden');
+    // Hide the welcome text and logo
+    document.querySelector('.container').style.opacity = '0';
     setTimeout(function() {
-        document.getElementById('mainContent').style.opacity = 1;
-    }, 100); // Timeout ensures the transition is triggered after the element becomes visible
+        document.getElementById('mainContent').classList.remove('hidden');
+        // Animate each card to scale up from 0 to 1
+        document.querySelectorAll('.card').forEach(function(card, index) {
+            setTimeout(function() { // Delay the animation for each card
+                card.style.transform = 'scale(1)';
+            }, index * 100); // Incremental delay for each card
+        });
+    }, 300); // Delay to allow the welcome text and logo to fade out
 });
+
