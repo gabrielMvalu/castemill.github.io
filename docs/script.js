4afb043b3fb58b6a4c0419d5209c271e93463c64
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-26 05:04:08 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,38 +1,43 @@
+document.getElementById('welcomeText').addEventListener('click', function() {
+    document.querySelector('.container').style.opacity = '0';
+    setTimeout(function() {
+        window.location.href = 'home.html';
+    }, 500);
+});
+
+window.onpageshow = function(event) {
+    if (event.persisted || (window.performance && window.performance.navigation.type == 2)) {
+        document.querySelector('.container').style.opacity = '1';
+    }
+};
+
 const cards = document.querySelectorAll(".card");
 
 cards.forEach((card) => {
   const height = card.clientHeight;
   const width = card.clientWidth;
 
-  const mouseMoveHandler = (evt) => {
-    evt.preventDefault();
-
-    requestAnimationFrame(() => {
-      const xRotation = -30 * ((evt.layerY - height / 2) / height);
-      const yRotation = 20 * ((evt.layerX - width / 2) / width);
-
-      card.style.transform = `perspective(1000px) scale(1.05) rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
-    });
-  };
-
-
   card.addEventListener("mouseenter", (evt) => {
-    evt.preventDefault();
-    card.addEventListener("mousemove", mouseMoveHandler);
+    card.addEventListener("mousemove", (evt) => {
+      requestAnimationFrame(() => {
+        const xRotation = -30 * ((evt.layerY - height / 2) / height);
+        const yRotation = 20 * ((evt.layerX - width / 2) / width);
+        card.style.transform = `perspective(1000px) scale(1.05) rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
+      });
+    });
   });
 
   card.addEventListener("mouseout", (evt) => {
-    evt.preventDefault();
     card.style.transform = "perspective(1000px) scale(1) rotateX(0) rotateY(0)";
     card.removeEventListener("mousemove", mouseMoveHandler);
   });
   
   card.addEventListener("click", (evt) => {
-    evt.preventDefault();
     card.style.animation = "spin 1s ease-in-out";
     setTimeout(() => {      
       card.style.animation = '';
     }, 1000);
   });
 });
+
 
