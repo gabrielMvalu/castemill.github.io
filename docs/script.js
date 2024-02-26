b4de6f8701dad6527c88102962dede540673011d
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-02-26 03:15:23 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,6 @@
 document.getElementById('welcomeText').addEventListener('click', function() {
-    // Animate the fade-out of the container
     document.querySelector('.container').style.opacity = '0';
     setTimeout(function() {
-        // Redirect to home.html after the fade-out
         window.location.href = 'home.html';
     }, 500); // Adjust timing as needed
 });
